bbd9596983ffa461ddb2d5a3be5ec1afd7538d8d
Jacob <jacobsaarela@gmail.com>
2024-08-08 15:45:28 +0200

    Add jokes

--- msg-gen.js
+++ msg-gen.js
@@ -1,7 +1,7 @@
 //This outputs a new, random message when ever the user uses it.
-const messages = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+const messagesStart = ["What do you call a bear", "Why shouldn't you trust a scientist", "What do you get when you cross an elephant and", "How does a penguin build", "Why don't skeletons fight"]
+const messageMid = [" with a plate of spaghetti?", " with a wooden leg.", " before the sun rises.", " using a spoon and a fork?", " with super glue?", " a pizza delivery?" ]
+const messageEnd = ["Pastacarnivore", "Because they might make light of the situation", "You get a wobbly trunk show!" , "With a lot of ice and some extra toppings!", 
+    "Because they don’t have the guts!" ]
 
-const generator = () => {
-    const output = messages.Math.floor(Math.random() * messages.length());
-    return console.log(output);
-}
+const generator = () => 
